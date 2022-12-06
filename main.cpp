2b05ff51c53af43f09901a43935c2bf0d8e4f072
tomorin <tanaka.t.bj@gmail.com>
2022-12-06 21:34:20 +0900

    Practice using Boolean.h is finished.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,34 @@
 #include <iostream>
-#include "Accounting.h"
-#include "IdNo.h"
+#include <string>
+#include "Boolean.h"
 
-using std::cout, std::endl;
+using std::cout, std::cin, std::endl;
+
+Boolean int_eq(int x, int y) {
+    return x == y;
+}
 
 int main() {
-    IdNo a;
-    IdNo b;
-    IdNo c[4];
+    int n;
+    Boolean a;
+    Boolean b = a;
+    Boolean c(100);
+    Boolean x[8];
 
-    cout << "a id: " << a.id() << endl;
-    cout << "b id: " << b.id() << endl;
-    for (int i=0; i<4; i++) {
-        std::cout << "c[" << i << "] id: " << c[i].id() << endl;
+    cout << "integer: ";
+    cin >> n;
+    x[0] = int_eq(n, 5);        // x[0]
+    x[1] = (n != 3);            // x[1]
+    x[2] = Boolean::False;      // x[2]
+    x[3] = 1000;                // x[3]
+    x[4] = c == Boolean::True;  // x[4]
+
+    cout << "value of a: " << int(a) << endl;
+    cout << "value of b: " << static_cast<const char*>(b) << endl;
+
+    for (int i = 0; i < 8; ++i) {
+        cout << "x[" << i << "] = " << x[i] << endl;
     }
-
-    cout << IdNo::get_max_id() << endl;
     
     return 0;
 }
